abedcbe174b0663852d221cf1b8ee0a60cb1b392
PrasanthBuddiga <85897250+PrasanthBuddiga@users.noreply.github.com>
2023-02-23 12:55:34 -0800

    Commiting only frontEnd code

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const schema=mongoose.Schema
-const userSchema=new schema({
-    email:String,
-    password:String
-})
-
-module.exports =mongoose.model('user',userSchema,'users')
